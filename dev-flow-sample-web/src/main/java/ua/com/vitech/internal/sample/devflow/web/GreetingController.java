662efef41e7f413c77b98db19c9bae2cbb65f7b8
igor <igor.kuzemko@vitech.com.ua>
2018-12-26 13:38:31 +0200

    Adding Cognito authentication

--- dev-flow-sample-web/src/main/java/ua/com/vitech/internal/sample/devflow/web/GreetingController.java
+++ dev-flow-sample-web/src/main/java/ua/com/vitech/internal/sample/devflow/web/GreetingController.java
@@ -3,11 +3,26 @@ package ua.com.vitech.internal.sample.devflow.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Principal;
+
 @RestController
 public class GreetingController {
 
     @RequestMapping("/greeting")
-    public String greeting() {
-        return "Hello";
+    public String greeting(Principal principal) {
+        return "Hello " + principal.getName();
+    }
+
+    @RequestMapping("/public")
+    public String publicPage() {
+        return "public page";
+    }
+
+    @RequestMapping("/login")
+    public void login(HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.sendRedirect("/oauth2/authorization/cognito-client-1");
     }
 }
