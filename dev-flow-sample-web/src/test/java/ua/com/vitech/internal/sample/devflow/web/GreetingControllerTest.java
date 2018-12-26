b96fb5136dd71f28ddaef72aaa8676190e2f8ef1
andrii.zaiats <andrii.zaiats@vitech.com.ua>
2018-12-26 15:43:23 +0200

    #392. Test actualized

--- dev-flow-sample-web/src/test/java/ua/com/vitech/internal/sample/devflow/web/GreetingControllerTest.java
+++ dev-flow-sample-web/src/test/java/ua/com/vitech/internal/sample/devflow/web/GreetingControllerTest.java
@@ -3,11 +3,18 @@ package ua.com.vitech.internal.sample.devflow.web;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.security.Principal;
+
 public class GreetingControllerTest {
 
     @Test
     public void testGreeting() {
-        Assert.assertEquals("Hello", new GreetingController().greeting());
+        Assert.assertEquals("Hello User", new GreetingController().greeting(new Principal() {
+            @Override
+            public String getName() {
+                return "User";
+            }
+        }));
     }
 
 }
