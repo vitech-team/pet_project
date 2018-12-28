cc2e1a60a5af46e4877d3d033e91038df39a65db
andrii.zaiats <andrii.zaiats@vitech.com.ua>
2018-12-28 14:18:11 +0200

    #413 Rest test added

--- dev-flow-sample-web/src/main/java/ua/com/vitech/internal/sample/devflow/web/config/SecurityConfig.java
+++ dev-flow-sample-web/src/main/java/ua/com/vitech/internal/sample/devflow/web/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package ua.com.vitech.internal.sample.devflow.web;
+package ua.com.vitech.internal.sample.devflow.web.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
---
+++ it/src/integration-test/java/ua/com/vitech/internal/sample/devflow/it/RestApi.java
@@ -0,0 +1,11 @@
+package ua.com.vitech.internal.sample.devflow.it;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RestApi {
+
+    @GET("public")
+    Call<String> publicUrl();
+
+}
