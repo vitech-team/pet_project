c01be8efd443ca647769e2dbc678b98a4b9a38df
root <vitaliy.mityevskyy@vitech.com.ua>
2019-01-10 13:32:14 +0200

    add cognito auth

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,17 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "cognito_secret" {}
+variable "app_image" {}
 
 variable "aws_region" {
   description = "The AWS region things are created in"
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 variable "az_count" {
   description = "Number of AZs to cover in a given region"
   default     = "2"
 }
-
-variable "app_image" {}
 
 variable "app_port" {
   description = "Port exposed by the docker image to redirect traffic to"
@@ -21,7 +20,7 @@ variable "app_port" {
 
 variable "app_count" {
   description = "Number of docker containers to run"
-  default     = 2
+  default     = 1
 }
 
 variable "ecs_autoscale_role" {
